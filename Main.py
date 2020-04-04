f01b6bcd640ae41ace7203bb52bc4a2ed69184cd
lukasberglund <lukastberglund@gmail.com>
2020-04-04 16:31:38 +0200

    slight changes

--- Main.py
+++ Main.py
@@ -11,17 +11,7 @@ from tqdm import tqdm # for showing progress
 
 
 # In[10]:
-
-
 data = pd.read_csv("training.csv")
-
-def increment_dict(d, key):
-    # if a key appears in the dictionary, increment its value, otherwise set the value at the key to one
-    if key in d:
-        d[key] += 1
-    else:
-        d[key] = 1
-    return d
 
 def sort_dict(d):
     return {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse = True)}
@@ -38,34 +28,40 @@ def top_entries(d, n):
 
 def normalize_occurences(word_occurences, articles):
     # convert total occurences to percentage occurence
+#     print(word_occurences)
     print(articles.shape[0])
     return {key : round(value / articles.shape[0], 2) for (key, value) in word_occurences.items()}
 
 topics = set(data.topic.values)
 
-all_words = set()
+all_unique_words = set()
 
 # getting the top words by topic
 for topic in topics:
     articles = data[data['topic'] == topic]
+
     word_count = {} #map each word to how often it appears in the topic's articles
+
     word_occurences = {} # map each word to how many articles it appears in
-    
+
     for i in articles.index:
         words = data['article_words'][i].split(',')
-        
+
         for word in words:
-            all_words.add(word)
-            word_count = increment_dict(word_count, word) #count another word appearing
-            
+            word_count.setdefault(word, 0)
+            word_count[word] += 1
+
         unique_words = set(words)
+        all_unique_words = all_unique_words.union(unique_words)
+
         for word in unique_words:
-            word_occurences = increment_dict(word_occurences, word)
-    
+            word_occurences.setdefault(word, 0)
+            word_occurences[word] += 1
+
     word_occurences = normalize_occurences(word_occurences, articles)
-            
+
     word_count = sort_dict(word_count)
-    
+
     print()
     print(topic)
     print("top word counts")
@@ -79,11 +75,4 @@ for topic in topics:
 
 
 
-print("total words:", len(all_words))
-
-
-# In[ ]:
-
-
-
-
+print("total words:", len(all_unique_words))
